f58242dbb78195ded5db2be71465b4dedbd89ce8
Yaroslav Holota <korova2321@gmail.com>
2020-11-27 10:58:51 +0200

    Add project files.

---
+++ USANationalDebt/APIManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace USANationalDebt
+{
+    public class APIManager
+    {
+        /*public long GetDebt()
+        {
+            
+        }
+        public int GetIncrement()
+        {
+            
+        }*/
+    }
+}
