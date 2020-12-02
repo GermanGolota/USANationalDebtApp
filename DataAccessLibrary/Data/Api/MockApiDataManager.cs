56e9807148990d670bb582a33e85a263b01fe5fd
Yaroslav Holota <korova2321@gmail.com>
2020-12-02 15:07:56 +0200

    Created api interface and a mock class

---
+++ DataAccessLibrary/Data/Api/MockApiDataManager.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.Data.Api
+{
+    //Class, that is used to test db system, that depends on the api, without actually implementing api
+    class MockApiDataManager : IApiDataManager
+    {
+        public List<DebtModel> GetDebtModels()
+        {
+            return new List<DebtModel> {
+                new DebtModel { Day = new DateTime(year:2020, month:4, day:12), Debt=200},
+                new DebtModel { Day = new DateTime(year:2020, month:4, day:13), Debt=205} ,
+                new DebtModel { Day = new DateTime(year:2020, month:4, day:14), Debt=212} ,
+                new DebtModel { Day = new DateTime(year:2020, month:4, day:15), Debt=214},
+                new DebtModel { Day = new DateTime(year:2020, month:4, day:18), Debt=220}
+            };
+        }
+    }
+}
