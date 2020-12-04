100836c1caef74fe41495df163e12b2bb843dc9c
Yaroslav Holota <korova2321@gmail.com>
2020-12-04 10:14:54 +0200

    Updated mock data to actually resemble actual data

--- DataAccessLibrary/Data/Api/MockApiDataManager.cs
+++ DataAccessLibrary/Data/Api/MockApiDataManager.cs
@@ -12,11 +12,12 @@ namespace DataAccessLibrary.Data.Api
         public async Task<List<DebtModel>> GetDebtModels()
         {
             List<DebtModel> models = new List<DebtModel> {
-                new DebtModel { Day = new DateTime(year:2020, month:4, day:12), Debt=200},
-                new DebtModel { Day = new DateTime(year:2020, month:4, day:13), Debt=205} ,
-                new DebtModel { Day = new DateTime(year:2020, month:4, day:14), Debt=212} ,
-                new DebtModel { Day = new DateTime(year:2020, month:4, day:15), Debt=214},
-                new DebtModel { Day = new DateTime(year:2020, month:4, day:18), Debt=220}
+                //2020-12-02
+                new DebtModel { Day = new DateTime(year:2020, month:12, day:02), Debt=21341926023922},
+                new DebtModel { Day = new DateTime(year:2020, month:11, day:30), Debt=21342192975117},
+                new DebtModel { Day = new DateTime(year:2020, month:11, day:27), Debt=21349471573310},
+                new DebtModel { Day = new DateTime(year:2020, month:11, day:25), Debt=21197640169859},
+                new DebtModel { Day = new DateTime(year:2020, month:11, day:24), Debt=21218611743445}
             };
             return models;
         }
