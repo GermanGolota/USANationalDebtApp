215438be9849d19a0e54761c219d05aa4ef611ef
Yaroslav Holota <korova2321@gmail.com>
2020-12-08 08:20:01 +0200

    created migrations

---
+++ DebtAPI/Migrations/20201207163809_InitialMigration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DebtAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DebtAPIInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Day = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Debt = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DebtAPIInfos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DebtInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Day = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Debt = table.Column<double>(type: "float", nullable: false),
+                    Increase = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DebtInfos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DebtAPIInfos");
+
+            migrationBuilder.DropTable(
+                name: "DebtInfos");
+        }
+    }
+}
