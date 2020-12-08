04ae8d4354a8875860f6be4effeb7f82924e1fd8
Yaroslav Holota <korova2321@gmail.com>
2020-12-08 08:19:46 +0200

    Realized data access interfaces using dbcontext

---
+++ DebtAPI/DB/EFClientRepo.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLibrary.Data.DB;
+using DataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DebtAPI.DB
+{
+    public class EFClientRepo : IClientAccess
+    {
+        private readonly DebtContext _context;
+
+        public EFClientRepo(DebtContext context)
+        {
+            this._context = context;
+        }
+        public async Task<DebtIncreaseModel> GetDebtInfo()
+        {
+            int max = _context.DebtInfos.Max(i => i.Id);
+            DebtIncreaseModel model = _context.DebtInfos.First(x => x.Id == max);
+            return model;
+        }
+    }
+}
---
+++ DebtAPI/DB/EFDebtRepo.cs
@@ -0,0 +1,45 @@
+﻿using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DebtAPI.DB
+{
+    public class EFDebtRepo : IDebtData
+    {
+        private readonly DebtContext _context;
+
+        public EFDebtRepo(DebtContext context)
+        {
+            this._context = context;
+        }
+        public async Task AddDebtToDB(DebtModel model)
+        {
+            _context.DebtAPIInfos.Add(model);
+            _context.SaveChanges();
+        }
+
+        public async Task CalculateAndInsertNewInfo()
+        {
+            List<DebtModel> debts = _context.DebtAPIInfos.ToList();
+            debts = debts.OrderBy((debt) => debt.Day).ToList();
+            DebtModel higher = debts.Last();
+            DebtModel lower = debts.First();
+            double diff = higher.Debt - lower.Debt;
+            TimeSpan time = higher.Day - lower.Day;
+            double increment = diff / time.TotalSeconds;
+            TimeSpan span = DateTime.Now - higher.Day;
+            double currentDebt = higher.Debt + span.TotalSeconds * increment;
+            DebtIncreaseModel model = new DebtIncreaseModel(DateTime.Now, currentDebt, increment);
+            _context.DebtInfos.Add(model);
+            _context.SaveChanges();
+        }
+
+        public async Task<List<DebtModel>> GetDebtsFromDB()
+        {
+            return _context.DebtAPIInfos.ToList();
+        }
+    }
+}
