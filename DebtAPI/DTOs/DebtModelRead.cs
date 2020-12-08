f2e90de6e84dd7fcd1940f00b2398970b71a8391
Yaroslav Holota <korova2321@gmail.com>
2020-12-08 08:18:30 +0200

    Created a dto

---
+++ DebtAPI/DTOs/DebtModelRead.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Models
+{
+    public class DebtModelRead
+    {
+        public DateTime Day { get; set; }
+        public double Debt { get; set; }
+        public double Increase { get; set; }
+    }
+}
