83111ba23be59d9b3800199f9e3c43ad33c82df0
Yaroslav Holota <korova2321@gmail.com>
2020-12-08 08:19:26 +0200

    Created db context

---
+++ DebtAPI/DB/DebtContext.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DebtAPI
+{
+    public class DebtContext:DbContext
+    {
+        public DebtContext(DbContextOptions<DebtContext> options):base(options)
+        {
+
+        }
+        public DbSet<DebtModel> DebtAPIInfos { get; set; }
+
+        public DbSet<DebtIncreaseModel> DebtInfos { get; set; }
+    }
+}
