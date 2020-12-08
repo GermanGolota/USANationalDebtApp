1a56a093310f8bd6443b04b8babd7802398a47f2
Yaroslav Holota <korova2321@gmail.com>
2020-12-08 08:15:48 +0200

    Separated models and decorated ones, that are used within the db

---
+++ DataAccessLibrary/Models/DBModels/DebtIncreaseModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataAccessLibrary.Models
+{
+    public class DebtIncreaseModel
+    {
+        public DebtIncreaseModel(DateTime day, double debt, double increase )
+        {
+            Day = day;
+            Debt = debt;
+            Increase = increase;
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime Day { get; set; }
+        [Required]
+        public double Debt { get; set; }
+        [Required]
+        public double Increase { get; set; }
+    }
+}
--- DataAccessLibrary/Models/DBModels/DebtModel.cs
+++ DataAccessLibrary/Models/DBModels/DebtModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace DataAccessLibrary.Models
@@ -11,7 +13,11 @@ namespace DataAccessLibrary.Models
             Day = day;
             Debt = debt;
         }
+        [Key]
+        public int Id { get; set; }
+        [Required]
         public DateTime Day { get; set; }
+        [Required]
         public double Debt { get; set; }
     }
 }
