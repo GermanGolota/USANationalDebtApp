b05e0c1f9ffa85d6e20674b5aed13d2fea7e7209
Yaroslav Holota <korova2321@gmail.com>
2020-12-10 10:06:02 +0200

    Revert "Updated controller to get new debt type"
    This reverts commit a65173f74f636b52afc28ddc82686fafc47a8900.

--- DebtAPI/Controllers/DebtController.cs
+++ DebtAPI/Controllers/DebtController.cs
@@ -2,7 +2,6 @@
 using DataAccessLibrary.Data.DB;
 using DataAccessLibrary.Models;
 using DataAccessLibrary.Models.DbModels;
-using DataAccessLibrary.Models.DBModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -24,27 +23,11 @@ namespace DebtAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<DebtModelRead>> GetExternalDebtModel()
+        public async Task<ActionResult<DebtModelRead>> GetDebtModel()
         {
-            ExternalIncreaseModel dbmodel = await _db.GetExternalDebtInfo();
+            InternalIncreaseModel dbmodel = await _db.GetExternalDebtInfo();
             //TODO:add automapper
             DebtModelRead model = new DebtModelRead {Day =dbmodel.Day,Debt= dbmodel.Debt,Increase=dbmodel.Increase };
-            if (model is not null)
-            {
-                return Ok(model);
-            }
-            else
-            {
-                return NotFound();
-            }
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<DebtModelRead>> GetInternalDebtModel()
-        {
-            InternalIncreaseModel dbmodel = await _db.GetInternalDebtInfo();
-            //TODO:add automapper
-            DebtModelRead model = new DebtModelRead { Day = dbmodel.Day, Debt = dbmodel.Debt, Increase = dbmodel.Increase };
             if (model is not null)
             {
                 return Ok(model);
