66e4d557676027ee89d28d44ad64f61721bd9c8a
Yaroslav Holota <korova2321@gmail.com>
2021-01-15 19:39:24 +0200

    SAFE! Moved migrations

---
+++ DataAccessLibrary/Migrations/20210115173759_MigrationsMove.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLibrary.Migrations
+{
+    public partial class MigrationsMove : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExternalDebtsAPI",
+                columns: table => new
+                {
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Debt = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExternalDebtsAPI", x => x.Time);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExternalDebtsInfo",
+                columns: table => new
+                {
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Debt = table.Column<double>(type: "float", nullable: false),
+                    Increase = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExternalDebtsInfo", x => x.Time);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InternalDebtsAPI",
+                columns: table => new
+                {
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Debt = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InternalDebtsAPI", x => x.Time);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InternalDebtsInfo",
+                columns: table => new
+                {
+                    Time = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Debt = table.Column<double>(type: "float", nullable: false),
+                    Increase = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InternalDebtsInfo", x => x.Time);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExternalDebtsAPI");
+
+            migrationBuilder.DropTable(
+                name: "ExternalDebtsInfo");
+
+            migrationBuilder.DropTable(
+                name: "InternalDebtsAPI");
+
+            migrationBuilder.DropTable(
+                name: "InternalDebtsInfo");
+        }
+    }
+}
