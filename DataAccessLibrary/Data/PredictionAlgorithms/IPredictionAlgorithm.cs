3f2acff0b1cfafabfcd120205ef3abb58b81a1c3
Yaroslav Holota <korova2321@gmail.com>
2021-01-29 16:38:45 +0200

    Created interface for prediction algorithms
    That is done, because prediction algorithm may varry and by abstracting it we can easily substitute a new one

---
+++ DataAccessLibrary/Data/PredictionAlgorithms/IPredictionAlgorithm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.Data
+{
+    public interface IPredictionAlgorithm
+    {
+        public double PredictValue(List<double> xValues, List<double> yValues, double inputValue);
+    }
+}
